33beea1ab7bc8476a2521116c574527f4b10a63b
Sandro Mani <manisandro@gmail.com>
2014-10-13 23:59:24 +0200

    detect-paraview-vtk

--- salome_adm/cmake_files/FindSalomeVTK.cmake
+++ salome_adm/cmake_files/FindSalomeVTK.cmake
@@ -16,55 +16,41 @@
 #
 # See http://www.salome-platform.org/ or email : webmaster.salome@opencascade.com
 #
-# Author: Adrien Bruneton
-#
 
-# VTK detection for Salome (see http://www.vtk.org/Wiki/VTK/Build_System_Migration)
-#
-#  !! Please read the generic detection procedure in SalomeMacros.cmake !!
-#
-
-# List the required components:
-SET(SalomeVTK_COMPONENTS 
-  vtkRenderingFreeTypeOpenGL
-  vtkRenderingLOD 
-  vtkRenderingAnnotation 
-  vtkFiltersParallel
-  vtkIOExport
-  vtkWrappingPythonCore  ## ParaView 4.0.1
-  #vtkWrappingPython
-  vtkIOXML
-  vtkFiltersVerdict
-  vtkRenderingLabel
-  vtkInteractionWidgets
-  vtkInfovisCore
-)
-
-#   Seting the following variable is equivalent to having passed the components
-#   when calling the FIND_PACKAGE() command.
-SET(SalomeVTK_FIND_COMPONENTS ${SalomeVTK_COMPONENTS})
-
-# If no VTK root dir is specified, try the ParaView root dir:
-SET(PARAVIEW_ROOT_DIR "$ENV{PARAVIEW_ROOT_DIR}" CACHE PATH "Path to the ParaView installation")
-IF(EXISTS "${PARAVIEW_ROOT_DIR}" AND (NOT VTK_ROOT_DIR))
-  MESSAGE(STATUS "Looking for VTK in the ParaView installation (PARAVIEW_ROOT_DIR exists and VTK_ROOT_DIR is not defined) ...")
+IF(EXISTS "/usr/share/cmake/paraview")
+  MESSAGE(STATUS "Found Paraview bundled VTK") 
+  SET(VTK_USE_FILE /usr/share/cmake/paraview/UseVTK.cmake)
+  SET(VTK_INCLUDE_DIRS /usr/include/paraview)
+  SET(VTK_LIBRARY_DIR /usr/lib${LIB_SUFFIX}/paraview)
   
-  # Extract sub-directory "paraview-x.xx":
-  FILE(GLOB VTK_DIR "${PARAVIEW_ROOT_DIR}/lib/cmake/paraview-[1-9].[0-9]*")
-  MESSAGE(STATUS "Setting VTK_DIR to: ${VTK_DIR}") 
-ENDIF()
+  FIND_LIBRARY(VTK_RENDERING_FREETYPEOPENGL vtkRenderingFreeTypeOpenGL ${VTK_LIBRARY_DIR})
+  FIND_LIBRARY(VTK_RENDERING_LOD vtkRenderingLOD ${VTK_LIBRARY_DIR})
+  FIND_LIBRARY(VTK_RENDERING_ANNOTATION vtkRenderingAnnotation ${VTK_LIBRARY_DIR})
+  FIND_LIBRARY(VTK_FILTERS_PARALLEL vtkFiltersParallel ${VTK_LIBRARY_DIR})
+  FIND_LIBRARY(VTK_IO_EXPORT vtkIOExport ${VTK_LIBRARY_DIR})
+  FIND_LIBRARY(VTK_WRAPPING_PYTHONCORE vtkWrappingPython27Core ${VTK_LIBRARY_DIR})
+  FIND_LIBRARY(VTK_IO_XML vtkIOXML ${VTK_LIBRARY_DIR})
+  FIND_LIBRARY(VTK_FILTERS_VERDICT vtkFiltersVerdict ${VTK_LIBRARY_DIR})
+  FIND_LIBRARY(VTK_RENDERING_LABEL vtkRenderingLabel ${VTK_LIBRARY_DIR})
+  FIND_LIBRARY(VTK_INTERACTION_WIDGETS vtkInteractionWidgets ${VTK_LIBRARY_DIR})
+  FIND_LIBRARY(VTK_INFOVIS_CORE vtkInfovisCore ${VTK_LIBRARY_DIR})
+  
+  SET(VTK_LIBRARIES
+    ${VTK_RENDERING_FREETYPEOPENGL}
+    ${VTK_RENDERING_LOD}
+    ${VTK_RENDERING_ANNOTATION}
+    ${VTK_FILTERS_PARALLEL}
+    ${VTK_IO_EXPORT}
+    ${VTK_WRAPPING_PYTHONCORE}
+    ${VTK_IO_XML}
+    ${VTK_FILTERS_VERDICT}
+    ${VTK_RENDERING_LABEL}
+    ${VTK_INTERACTION_WIDGETS}
+    ${VTK_INFOVIS_CORE})
 
-SALOME_FIND_PACKAGE_AND_DETECT_CONFLICTS(VTK VTK_INCLUDE_DIRS 2)
-MARK_AS_ADVANCED(VTK_DIR)
-
-IF(VTK_FOUND)
-  MESSAGE(STATUS "VTK version is ${VTK_MAJOR_VERSION}.${VTK_MINOR_VERSION}")
-  #MESSAGE(STATUS "VTK libraries are: ${VTK_LIBRARIES}")
-ENDIF()
-
-IF(VTK_FOUND) 
-  SALOME_ACCUMULATE_HEADERS(VTK_INCLUDE_DIRS)
-  SALOME_ACCUMULATE_ENVIRONMENT(LD_LIBRARY_PATH ${VTK_LIBRARIES})
-  SALOME_ACCUMULATE_ENVIRONMENT(PYTHONPATH ${VTK_LIBRARIES})
-  SALOME_ACCUMULATE_ENVIRONMENT(PV_PLUGIN_PATH ${VTK_LIBRARIES})
+  SALOME_ACCUMULATE_HEADERS(${VTK_INCLUDE_DIRS})
+  SALOME_ACCUMULATE_ENVIRONMENT(LD_LIBRARY_PATH ${VTK_LIBRARY_DIR})
+  SALOME_ACCUMULATE_ENVIRONMENT(PYTHONPATH ${VTK_LIBRARY_DIR})
+  SALOME_ACCUMULATE_ENVIRONMENT(PV_PLUGIN_PATH ${VTK_LIBRARY_DIR})
+  SET(VTK_FOUND TRUE)
 ENDIF()
